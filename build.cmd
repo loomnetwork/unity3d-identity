55e4d23dae405135a8ae465c9f4cc67e5de40dc0
Dmitry Khodarenok <dkhodarenok@gmail.com>
2018-05-25 10:38:52 +0700

    Unity package build scripts

---
+++ build.cmd
@@ -0,0 +1,2 @@
+C:\Program Files\Unity\Editor\Unity.exe -projectPath "%~dp0" -batchmode -quit -exportPackage "Assets"  unity3d-identity.unitypackage
+
---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -projectPath "$PWD" -batchmode -quit -exportPackage "Assets"  unity3d-identity.unitypackage
